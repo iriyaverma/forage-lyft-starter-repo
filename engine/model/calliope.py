80176edc54e93a032b11f3872ae9242c9527fb20
Riya Verma <117571193+iriyaverma@users.noreply.github.com>
2023-07-24 23:43:20 +0530

    Update calliope.py

--- engine/model/calliope.py
+++ engine/model/calliope.py
@@ -1,7 +1,12 @@
 from datetime import datetime
 from engine.capulet_engine import CapuletEngine
+from engine.willoughby_engine import WilloughbyEngine
+from engine.sternman_engine import SternmanEngine
 
-class Calliope(CapuletEngine):
-    def needs_service(self) -> bool:
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+class CarEngine:
+    def __init__(self, last_service_date):
+        self.last_service_date = last_service_date
+
+    def needs_service(self, threshold_years: int) -> bool:
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + threshold_years)
         return service_threshold_date < datetime.today().date() or self.engine_should_be_serviced()
