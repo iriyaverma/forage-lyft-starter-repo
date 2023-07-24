42975e33d3f80125852815c9e17b8d6fc5caa35a
Riya Verma <117571193+iriyaverma@users.noreply.github.com>
2023-07-24 23:26:40 +0530

    Update car.py

--- car.py
+++ car.py
@@ -1,9 +1,13 @@
 from abc import ABC, abstractmethod
+from datetime import date
 
 class Car(ABC):
-    def __init__(self, last_service_date):
+    def __init__(self, last_service_date: date):
         self.last_service_date = last_service_date
 
     @abstractmethod
-    def needs_service(self):
-        raise NotImplementedError("Subclasses must implement this method.")
+    def needs_service(self) -> bool:
+        pass
+
+    def engine_should_be_serviced(self) -> bool:
+        return self.needs_service()
